7527ba259466f26ab16bc8f915d59260893867f0
iuwqyir <toomas.oosalu@gmail.com>
2023-05-13 17:10:12 +0100

    actually save rollup to supabase

--- tasks/detect-rollups.js
+++ tasks/detect-rollups.js
@@ -1,10 +1,11 @@
 const detectRollups = require('../lib/rollups/detect')
 const enrichConfig = require('../lib/rollups/enrichConfig')
+const saveRollup = require('../lib/rollups/save')
 
 exports.handler = async () => {
   let rollups = await detectRollups();
   for (const rollup of rollups) {
     await enrichConfig(rollup);
   }
-  console.log(rollups)
+  await Promise.all(rollups.map(rollup => saveRollup(rollup)))
 };
