3be85d29dcc6e81a3e3706a936e97a64e4dad470
Amine Afia <afia.amine@gmail.com>
2023-05-13 21:16:57 +0100

    removed keys oops

--- opti-mystic-ui/util/supabase.js
+++ opti-mystic-ui/util/supabase.js
@@ -1,8 +1,8 @@
 const {createClient} = require("@supabase/supabase-js")
 
 const supabase = createClient(
-	"https://twoiypbxxgynzsosvpuj.supabase.co",
-	"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR3b2l5cGJ4eGd5bnpzb3N2cHVqIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODM5ODYwMTMsImV4cCI6MTk5OTU2MjAxM30.6txH8P3SIk0RlWvbpU7oTmtHY_jiyFiZwfO5OzJV9aM"
+	process.env.SUPABASE_URL,
+	process.env.SUPABASE_API_KEY
 )
 
 module.exports = {supabase}
