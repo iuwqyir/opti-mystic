2c0e11bdd020436529424fb0e2cd450add0b232c
iuwqyir <toomas.oosalu@gmail.com>
2023-05-13 18:55:36 +0100

    detect network name dynamically

--- backend/lib/rollups/save.js
+++ backend/lib/rollups/save.js
@@ -1,9 +1,11 @@
 const { supabase } = require('../connections/supabase')
+const { alchemy } = require('../connections/alchemy')
 const crypto = require('crypto');
 
 module.exports = async (rollup) => {
   if (!rollup) return
 
+  const network = await alchemy.core.getNetwork()
   const contracts = JSON.stringify(rollup.created)
   const rollupData = {
     admin: rollup.admin,
@@ -11,7 +13,7 @@ module.exports = async (rollup) => {
     batcher: rollup.batcher,
     sequencer: rollup.sequencer,
     l1_start_time: rollup.blockTimestamp,
-    l1_chain_name: 'görli', // TODO
+    l1_chain_name: network.name,
     contracts,
     block_hash: rollup.blockHash,
     contracts_hash: crypto.createHash('sha256').update(contracts).digest('hex')
